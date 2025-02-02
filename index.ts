4e6ce7f9797f732a2f50f85b4398bb1262bb455f
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2025-02-02 09:22:22 +0700

    Basic view

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import express, { Express, Request, Response } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express();
-const port: number | string = 3000;
+const port: number | string = process.env.PORT || 3000;
+
+database.connect();
 
 app.set("views", "./views");
 app.set("view engine", "pug");
